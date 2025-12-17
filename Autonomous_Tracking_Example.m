7a3895d5caf7de427197ceee0968f069ad0f7165
ParkingTom <geonhopark@gmail.com>
2025-12-16 21:31:36 -0800

    Delete Autonomous_Tracking_Example.m

--- Autonomous_Tracking_Example.m
+++
@@ -1,7 +0,0 @@
-%% This is an example to run the autonomous artery tracking algorithm.
-
-if exist('Sample.mp4', 'file') == 0
-    disp('Sample video not in the directory!')
-end
-
-Autonomous_tracking_algorithm
